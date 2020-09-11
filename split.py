a9191ac15dc5fb1f0078f4ebaedbab32b401efdf
varant <varantarabian@gmail.com>
2020-09-10 23:35:29 -0700

    update split.py

--- split.py
+++ split.py
@@ -2,56 +2,115 @@ import os
 import sys
 import random
 import shutil
+import argparse
+import csv
+from itertools import islice
+from itertools import chain
+from time import sleep
+
+####################
+
+# https://stackoverflow.com/questions/24527006/split-a-generator-into-chunks-without-pre-walking-it/24527424
 
 
-# create directories
-dir_name = "data"
-if os.path.isdir(dir_name) is False:
-  os.makedirs(dir_name)
-  os.makedirs(dir_name + '/train/mask')
-  os.makedirs(dir_name + '/train/no-mask')
-  os.makedirs(dir_name + '/valid/mask')
-  os.makedirs(dir_name + '/valid/no-mask')
-  os.makedirs(dir_name + '/test/mask')
-  os.makedirs(dir_name + '/test/no-mask')
-else:
-  sys.exit()
+def chunks(iterable, size=10):
+    iterator = iter(iterable)
+    for first in iterator:
+        yield chain([first], islice(iterator, size - 1))
 
-move images
-print("moving images")
 
-def move(loc, dest, files):
-  for file in files:
-    shutil.move(os.path.join(loc, file), os.path.join(dest, file))
-    print(file, end="\r", flush=True)
+def copy(loc, dest, files):
+    n = len(files)
+    i = 0
+    for file in files:
+        shutil.copy(os.path.join(loc, file), os.path.join(dest, file))
+        i += 1
+        print("Moving file %d/%d     " % (i, n), end="\r", flush=True)
 
-cwd = os.getcwd()
 
-# mask
-path = os.path.join(cwd, "data/train/mask")
-dir_list = os.listdir(path)
-n = len(dir_list)
-random.shuffle(dir_list)
+def main():
+    parser = argparse.ArgumentParser(
+        description="splits images into random partitions")
+    parser.add_argument("images", help="path to directory containing images")
+    parser.add_argument(
+        "csv", nargs='?', help="path to csv file containing labels")
+    parser.add_argument("-l", "--labels", nargs='+', type=str,
+                        help="creates a subfolder in train and valid for each label")
+    parser.add_argument("-s", "--size", type=float,
+                        help="train percentage (between 0 and 1)")
+    args = parser.parse_args()
 
-nvalid = int((pvalid / 100) * n)
-new_path = os.path.join(cwd, "data/valid/mask")
-move(path, new_path, dir_list[0:nvalid])
+    if args.labels and not args.csv:
+        print("must specify path to csv file")
+        sys.exit()
 
-ntest = int((ptest / 100) * n)
-new_path = os.path.join(cwd, "data/test/mask")
-move(path, new_path, dir_list[nvalid:(nvalid+ntest)])
+    cwd = os.getcwd()
+    out_dir = "out2"
+    sub_dirs = ["train", "valid"]
 
-# no mask
-path = os.path.join(cwd, "data/train/no-mask")
-dir_list = os.listdir(path)
-n = len(dir_list)
-random.shuffle(dir_list)
+    if args.size:
+        train_size = args.size
+    else:
+        train_size = 0.8
 
-nvalid = int((pvalid / 100) * n)
-new_path = os.path.join(cwd, "data/valid/no-mask")
-move(path, new_path, dir_list[0:nvalid])
+    if not os.path.isdir(args.images):
+        sys.exit()
 
-ntest = int((ptest / 100) * n)
-new_path = os.path.join(cwd, "data/test/no-mask")
-move(path, new_path, dir_list[nvalid:(nvalid+ntest)])
+    if os.path.isdir(out_dir):
+        shutil.rmtree(out_dir)
 
+    os.makedirs(out_dir)
+    os.chdir(out_dir)
+
+    for sub_dir in sub_dirs:
+        os.makedirs(sub_dir)
+        if args.labels:
+            for lbl in args.labels:
+                os.makedirs(sub_dir + "/" + lbl)
+
+    os.chdir(cwd)
+
+    if not args.labels:
+        lst = os.listdir(args.images)
+        n = len(lst)
+        random.shuffle(lst)
+
+        ntrain = int(train_size * n)
+        train = lst[0:ntrain]
+        valid = lst[ntrain:]
+
+        copy(args.images, out_dir + "/train", train)
+        copy(args.images, out_dir + "/valid", valid)
+    else:
+        with open(args.csv) as fp:
+            reader = csv.reader(fp, delimiter=",", quotechar='"')
+            fields = next(reader, None)
+            indexClass = fields.index('classname')
+            n = sum(1 for row in reader if row[indexClass] in args.labels)
+
+        with open(args.csv) as fp:
+            reader = csv.reader(fp, delimiter=",", quotechar='"')
+
+            fields = next(reader, None)
+            indexName = fields.index('name')
+            indexClass = fields.index('classname')
+
+            ntrain = int(train_size * n)
+
+            i = 0
+            for row in reader:
+                label = row[indexClass]
+                if label in args.labels:
+                    name = row[indexName]
+                    dest = "%s/%s/%s/%s" % (out_dir, "train" if i <
+                                            ntrain else "valid", label, name)
+                    shutil.copy(os.path.join(args.images, name), dest)
+                    i += 1
+                    print("%d/%d" % (i, n), end="\r", flush=True)
+
+            print()
+
+
+####################
+if __name__ == "__main__":
+    main()
